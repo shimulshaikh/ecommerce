d1d7d3b9bd02218141f54aa3d606d760843f20ee
shimulshaikh <shimulshaikhcse@gmail.com>
2020-12-26 16:43:53 +0600

    header menu dynamic

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Section;
 
 class IndexController extends Controller
 {
     public function index()
     {
     	$page_name = "Index";
-    	return view('front.index')->with(compact('page_name'));
+
+    	$sections = Section::with('categories')->where('status',1)->get();
+    	// $sections = json_decode(json_encode($sections),true);
+     //    echo "<pre>"; print_r($sections); die;
+
+    	return view('front.index')->with(compact('page_name','sections'));
     }
 }
